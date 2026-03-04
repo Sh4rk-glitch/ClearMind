7e37b433cedd7dcecb72782cde9b189620bb0b48
Shourya Mishra <shouryamishra011@gmail.com>
2026-03-04 18:30:00 -0500

    Fixing notification import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import { NotificationProvider } from './components/Notification.tsx';
+import { NotificationProvider } from './components/Notification';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
